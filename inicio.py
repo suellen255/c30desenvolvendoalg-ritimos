f5d4f6ecf21c73ce8c4166eb3c4cd05e967c79b3
suellen255 <anasuellen31@gmail.com>
2026-02-25 17:24:32 -0300

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,10 @@
+nome= "Susu"
+idade= 16
+altura= 1,59
+matriculado= True
+
+
+print(nome)
+print(idade)
+print(altura)
+print(matricula)
---
+++ váriaveis.py
@@ -0,0 +1,5 @@
+var1 = 10
+Var1 = 20
+
+print(var1)
+print(Var1)
