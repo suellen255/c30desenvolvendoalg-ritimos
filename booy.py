2045c5c7b325076c41d9ce51009d54b383284eeb
suellen255 <anasuellen31@gmail.com>
2026-02-26 08:38:13 -0300

    Add files via upload

---
+++ booy.py
@@ -0,0 +1,6 @@
+resp = input("Você vai passar de ano? /N:").strip().lower()
+
+resultado = (resp == "s")
+
+print("Resultado", resultado)
+print(type(resultado))
---
+++ explo.py
@@ -0,0 +1,6 @@
+base = input("Digite a base:")
+altura = input("Digite a altura:")
+
+area = base * altura
+
+print(area)
